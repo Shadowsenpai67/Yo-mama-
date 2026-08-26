68d4ebd9c8729a09d313fc0ed0254c2031caa17d
Shadowsenpai67 <samsungxipone@gmail.com>
2026-08-26 13:03:38 +0100

    Add AniPulse login and signup page

---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+import { Suspense } from 'react'
+import AuthForm from '@/components/AuthForm'
+
+export default function LoginPage() {
+  return <main className="authPage"><Suspense fallback={<div className="authCard">Loading…</div>}><AuthForm /></Suspense></main>
+}
